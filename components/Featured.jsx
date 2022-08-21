f3e480db54b1c1844b5c9099ca3b5fe737e8605e
Fan <aliciafzhang@gmail.com>
2022-08-21 16:05:32 -0700

    image gallery set up

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,23 +1,35 @@
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
-
+import {useState} from "react";
 const Featured = () => {
+  const [index, setIndex] = useState(0);
   const images = [
     "/img/featured.png",
     "/img/featured2.png",
     "/img/featured3.png"
   ];
+  const handleArrow = (direction) => {
+    if(direction === "l") {
+      setIndex(index !== 0? index-1: 2);
+    } else {
+      setIndex(index !==2? index+1: 0);
+    }
+  }
   return (
     <div className={styles.container}>
-      <Image src="/img/arrowl.png" alt="" width="20" height="20"/>
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          {images.map(((img, i)=>
-          <Image src="/img/featured.png" key={i} alt="" layout='fill'/> ))}
-
-        </div>
+      <div className={styles.arrowContainer} style={{left:0}} onClick={()=> handleArrow("l")}>
+      <Image src="/img/arrowl.png" alt="" layout='fill' objectFit="contain"/>
       </div>
-      <Image src="/img/arrowr.png" alt="" width="20" height="20"/>
+      <div className={styles.wrapper} style={{transform: `translateX(${-100*index}vw)`}}>
+        {images.map(((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout='fill'/>
+          </div>)
+        ))}
+      </div>
+      <div className={styles.arrowContainer} style={{right:0}} onClick={()=> handleArrow("r")}>
+      <Image src="/img/arrowr.png" alt=""  layout='fill' objectFit="contain"/>
+      </div>
     </div>
   )
 }
